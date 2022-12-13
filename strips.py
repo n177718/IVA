bf44ea47475f50ed0c07112fcfecc11367d5398f
Bill <l.m.z6677hotmail@gmail.com>
2022-12-12 19:48:42 -0500

    Update strips.py

--- strips.py
+++ strips.py
@@ -594,6 +594,7 @@ def find_IE(solution,world,precondition):
     result_SE = SE[0]
     IE = SE[0]
     causality = 0
+    tmp_IE = SE[0]
     for i in SE:
         tmp_IE,tmp_causality = find_previous(solution,i,precondition)
         print(tmp_causality)
